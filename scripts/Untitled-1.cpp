403e514ad11dd579e20391ad4db6209911ce48c0
Natali-39 <nataniks39@gmail.com>
2026-03-17 17:06:40 +0300

    dfg

---
+++ scripts/Untitled-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+#include <string>
+#include <ctime>
+
+using namespace std;
+int main()
+{
+    int v = 5;
+    int mas[3] = {1, 7, 9};
+    int *pl = &v;
+    cout << v << "\n";
+    cout << &v << "\n";
+}
